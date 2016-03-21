f357e45848e6e778d9c00ac6965d52554933bdc5
isabellaHea <isabella1217@163.com>
2016-03-21 16:55:26 +0800

    Initial Commit

---
+++ IncreasedInfluence/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IncreasedInfluence
+//
+//  Created by isahuang on 16/3/21.
+//  Copyright © 2016年 isahuang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ IncreasedInfluence/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  IncreasedInfluence
+//
+//  Created by isahuang on 16/3/21.
+//  Copyright © 2016年 isahuang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
