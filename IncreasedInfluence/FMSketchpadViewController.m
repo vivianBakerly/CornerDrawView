f28e2ce8728df6ee267f620036d8b2ea96b880a4
vivianBakerly <vivian_sa17@163.com>
2016-04-15 23:15:32 +0800

    complete sketchpadViewController

--- IncreasedInfluence/FMSketchpadViewController.m
+++ IncreasedInfluence/FMSketchpadViewController.m
@@ -17,6 +17,12 @@ static const CGFloat kSketchpadGap = 50;
 @property(nonatomic, strong) UITextView *radiusTextView;
 @property(nonatomic, strong) UITextView *borderWidthTextView;
 @property(nonatomic, strong) FMRadiusImageView *radiusImageView;
+@property(nonatomic, strong) UIButton *confirmBtn;
+@property(nonatomic, assign) BOOL turnOn;
+
+
+@property(nonatomic, assign)CGFloat radiusDraft;
+@property(nonatomic, assign)CGFloat widthDraft;
 
 @end
 @implementation FMSketchpadViewController
@@ -26,6 +32,9 @@ static const CGFloat kSketchpadGap = 50;
     [super viewDidLoad];
     [self initNormalViews];
     [self initRadiusViews];
+    [self initColorSwitcher];
+    [self initConfirmBtn];
+    [self addGesture];
 }
 
 #pragma mark init methods
@@ -47,7 +56,8 @@ static const CGFloat kSketchpadGap = 50;
     
     self.radiusTextView = [[UITextView alloc] initWithFrame:CGRectMake(startX, self.sketchpadView.frame.origin.y + sketchPadWidth + 20, textViewWidth, textViewHeight)];
     self.radiusTextView.backgroundColor = [UIColor grayColor];
-    
+    self.radiusTextView.delegate = self;
+    self.radiusTextView.keyboardType = UIKeyboardTypeDecimalPad;
     
     UILabel *borderLabel = [[UILabel alloc] initWithFrame:CGRectMake(kSketchpadGap, self.radiusTextView.frame.origin.y + textViewHeight + 20, textViewWidth * 2, textViewHeight)];
     borderLabel.text = @"Enter borderWidth: ";
@@ -56,6 +66,8 @@ static const CGFloat kSketchpadGap = 50;
     
     self.borderWidthTextView = [[UITextView alloc] initWithFrame:CGRectMake(startX, self.radiusTextView.frame.origin.y + textViewHeight + 20, textViewWidth, textViewHeight)];
     self.borderWidthTextView.backgroundColor = [UIColor grayColor];
+    self.borderWidthTextView.delegate = self;
+    self.borderWidthTextView.keyboardType = UIKeyboardTypeDecimalPad;
     [self.view addSubview:self.radiusTextView];
     [self.view addSubview:self.borderWidthTextView];
 }
@@ -69,4 +81,72 @@ static const CGFloat kSketchpadGap = 50;
     self.radiusImageView.image =  [UIImage imageNamed:@"Avatar_2"];
     [self.view addSubview:self.radiusImageView];
 }
+
+- (void)initColorSwitcher
+{
+    CGFloat middle = [UIScreen mainScreen].bounds.size.width / 2;
+    CGFloat switchWidth = 51;
+    UISwitch *switcher = [[UISwitch alloc] initWithFrame:CGRectMake(middle - switchWidth / 2, self.borderWidthTextView.frame.origin.y + self.borderWidthTextView.frame.size.height + 30, switchWidth, switchWidth / 2)];
+    [switcher addTarget:self action:@selector(switchMode) forControlEvents:UIControlEventValueChanged];
+    [self.view addSubview:switcher];
+    
+    CGFloat labelWidth = 50;
+    UILabel *left = [[UILabel alloc] initWithFrame:CGRectMake(switcher.frame.origin.x - 10 - labelWidth, switcher.frame.origin.y, labelWidth, switcher.frame.size.height)];
+    left.text = @"RED";
+    left.textColor = [UIColor redColor];
+    [self.view addSubview:left];
+    
+    UILabel *right = [[UILabel alloc] initWithFrame:CGRectMake(switcher.frame.origin.x + switchWidth + 10, switcher.frame.origin.y, labelWidth, switcher.frame.size.height)];
+    right.text = @"YELLOW";
+    right.textColor = [UIColor yellowColor];
+    [self.view addSubview:right];
+}
+
+- (void)initConfirmBtn
+{
+    CGFloat middle = [UIScreen mainScreen].bounds.size.width / 2;
+    CGFloat btnWidth = 250;
+    self.confirmBtn = [[UIButton alloc] initWithFrame:CGRectMake(middle - btnWidth / 2, self.borderWidthTextView.frame.origin.y + self.borderWidthTextView.frame.size.height + 30 + 33 + 20, btnWidth, 50)];
+    self.confirmBtn.backgroundColor = [UIColor yellowColor];
+    self.confirmBtn.layer.cornerRadius = 10;
+    [self.confirmBtn setTitle:@"Confirm" forState:UIControlStateNormal];
+    [self.confirmBtn setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+    [self.confirmBtn addTarget:self action:@selector(pressConfirm) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:self.confirmBtn];
+}
+
+#pragma mark Actions
+- (void)pressConfirm
+{
+    self.radiusImageView.cornerRadius = self.radiusDraft;
+    self.radiusImageView.borderWidth = self.widthDraft;
+    self.radiusImageView.borderColor = (self.turnOn) ? [UIColor redColor] : [UIColor yellowColor];
+}
+- (void)switchMode
+{
+    _turnOn = !_turnOn;
+}
+
+#pragma textView methods
+-(void)addGesture
+{
+    UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(resignResponders)];
+    [self.view addGestureRecognizer:tap];
+}
+
+-(void)textViewDidEndEditing:(UITextView *)textView
+{
+    if(textView == self.radiusTextView){
+        self.radiusDraft = [textView.text floatValue];
+    }else if(textView == self.borderWidthTextView){
+        self.widthDraft = [textView.text floatValue];
+    }
+}
+//点击View任意空白处，关闭键盘
+- (void)resignResponders {
+    
+    [self.radiusTextView resignFirstResponder];
+    [self.borderWidthTextView resignFirstResponder];
+}
+
 @end
---
+++ IncreasedInfluence/YYSentinel.h
@@ -0,0 +1,61 @@
+//
+//  YYSentinel.h
+//  YYKit <https://github.com/ibireme/YYKit>
+//
+//  Created by ibireme on 15/4/13.
+//  Copyright (c) 2015 ibireme.
+//
+//  This source code is licensed under the MIT-style license found in the
+//  LICENSE file in the root directory of this source tree.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+#import <libkern/OSAtomic.h>
+
+/// Global display queue, used for content rendering.
+static dispatch_queue_t YYAsyncLayerGetDisplayQueue() {
+#define MAX_QUEUE_COUNT 16
+    static int queueCount;
+    static dispatch_queue_t queues[MAX_QUEUE_COUNT];
+    static dispatch_once_t onceToken;
+    static int32_t counter = 0;
+    dispatch_once(&onceToken, ^{
+        queueCount = (int)[NSProcessInfo processInfo].activeProcessorCount;
+        queueCount = queueCount < 1 ? 1 : queueCount > MAX_QUEUE_COUNT ? MAX_QUEUE_COUNT : queueCount;
+        if ([UIDevice currentDevice].systemVersion.floatValue >= 8.0) {
+            for (NSUInteger i = 0; i < queueCount; i++) {
+                dispatch_queue_attr_t attr = dispatch_queue_attr_make_with_qos_class(DISPATCH_QUEUE_SERIAL, QOS_CLASS_USER_INITIATED, 0);
+                queues[i] = dispatch_queue_create("com.ibireme.yykit.render", attr);
+            }
+        } else {
+            for (NSUInteger i = 0; i < queueCount; i++) {
+                queues[i] = dispatch_queue_create("com.ibireme.yykit.render", DISPATCH_QUEUE_SERIAL);
+                dispatch_set_target_queue(queues[i], dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0));
+            }
+        }
+    });
+    int32_t cur = OSAtomicIncrement32(&counter);
+    if (cur < 0) cur = -cur;
+    return queues[(cur) % queueCount];
+#undef MAX_QUEUE_COUNT
+}
+
+NS_ASSUME_NONNULL_BEGIN
+
+/**
+ YYSentinel is a thread safe incrementing counter. 
+ It may be used in some multi-threaded situation.
+ */
+@interface YYSentinel : NSObject
+
+/// Returns the current value of the counter.
+@property (readonly) int32_t value;
+
+/// Increase the value atomically.
+/// @return The new value.
+- (int32_t)increase;
+
+@end
+
+NS_ASSUME_NONNULL_END
