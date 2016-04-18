ec361f130da521311472f61cb3b173b5c758f7c5
vivianBakerly <vivian_sa17@163.com>
2016-04-18 14:39:47 +0800

    adjust files structure

---
+++ IncreasedInfluence/FMComparisonCell.h
@@ -0,0 +1,16 @@
+//
+//  FMComparisonCell.h
+//  IncreasedInfluence
+//
+//  Created by isahuang on 16/4/18.
+//  Copyright © 2016年 isahuang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FMComparisonCell : UITableViewCell
+
++(NSString *)identifier;
++(CGFloat)heightForRow;
+-(void)setupItem;
+@end
